e03696bc2f3034c99666adcd15c66715593f1283
Mikhail <mihail_9000@mail.ru>
2023-01-19 06:38:35 +0300

    ...

--- Model/RelayCommand.cs
+++ Model/RelayCommand.cs
@@ -7,17 +7,19 @@ namespace Sclad.Model
     {
         private Action<object> _execute;
         private Func<object, bool> _canExecute;
+
         public event EventHandler CanExecuteChanged
         {
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
         }
 
-        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
         {
             _execute = execute;
             _canExecute = canExecute;
         }
+
         public bool CanExecute(object parameter)
         {
             return _canExecute == null || _canExecute(parameter);
