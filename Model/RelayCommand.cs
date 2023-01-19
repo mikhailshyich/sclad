eb8ec0d5dfd19f23d0485287b2444411c8ef5b36
Mikhail <mihail_9000@mail.ru>
2023-01-19 05:48:12 +0300

    .

---
+++ Model/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Sclad.Model
+{
+    internal class RelayCommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+        private Action<object> value;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<object> value)
+        {
+            this.value = value;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
---
+++ ViewModel/DataManageVM.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Sclad.Model;
+using Sclad.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sclad.ViewModel
+{
+    public class DataManageVM : INotifyPropertyChanged
+    {
+        // все товары
+        private List<Goods> allGoods = DataWorker.GetGoods();
+        public List<Goods> AllGoods
+        {
+            get { return allGoods; }
+            set
+            {
+                allGoods = value;
+                NotifyPropertyChanged("AllGoods");
+            }
+        }
+
+        //команды открытия окон
+        private RelayCommand? openAddNewGoodsWnd;
+        public RelayCommand OpenAddNewGoodsWnd
+        {
+            get
+            {
+                return openAddNewGoodsWnd ?? new RelayCommand(obj => { OpenAddGoodsWindowMethod(); });
+            }
+        }
+
+        //методы открытия окон
+        private void OpenAddGoodsWindowMethod()
+        {
+            AddNewGoods newGoodsWindow = new AddNewGoods();
+            SetCenterPositionAndOpen(newGoodsWindow);
+        }
+
+        private void SetCenterPositionAndOpen(Window window)
+        {
+            window.Owner = Application.Current.MainWindow;
+            window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            window.ShowDialog();
+        }
+            
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String propertyTitle)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyTitle));
+            }
+        }
+    }
+}
